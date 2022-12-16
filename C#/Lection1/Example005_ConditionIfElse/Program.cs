293875e64b2cd9e22fcc774070c294aa55815b79
PoshLick <timmer0007@mail.ru>
2022-12-16 22:48:47 +0300

    Приветственное окошко

---
+++ C#/Lection1/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "аня")
+{
+    Console.WriteLine("Кто это у нас тут? Это же Анюта!");
+}
+else
+{
+    Console.Write("Ты не Аня, пошёл нах#й, ");
+    Console.WriteLine(username);
+}
